f73728b44912c7ee577d7fec96394fab52409551
Ricco Amezcua <ricco.amezcua@gmail.com>
2026-04-01 13:29:35 -0500

    Finish main page

--- submission-site/app/layout.tsx
+++ submission-site/app/layout.tsx
@@ -13,7 +13,119 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
+      <head>
+            <meta charSet="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta httpEquiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+    <meta httpEquiv="Keywords" name="Keywords" content="Betty Award, writing contest, children writing contest, writing contest kids, writing contest prize"/>
+    <meta name="description" content="The Betty Award is a writing contest for children ages 8-12. Kids can enter their stories for a chance to win cash prizes."/>
+    <meta name="classification" content="Writing Contest"/>
+    <meta name="robots" content="ALL"></meta>
+
+    
+    <title>The Betty Award</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=PT+Serif&display=swap" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/polyfill/v3/polyfill.min.js?version=3.52.1&features=fetch"></script>
+    <script src="https://kit.fontawesome.com/a6313af583.js" crossOrigin="anonymous"></script>
+      </head>
+
+      <body className="bg-white flex flex-col h-screen justify-between">
+    <nav className="bg-white shadow-lg">
+        <div className="max-w-6xl mx-auto px-4">
+            <div className="flex justify-between">
+                <div className="flex space-x-7">
+                    <div>
+                        {/* <!-- Website Logo --> */}
+                        <a href="/" className="flex items-center py-4 px-2">
+                            <img src="https://d30tirrb1lsi4u.cloudfront.net/public/logo.gif" alt="Logo" className="h-16  mr-2"/>
+                        </a>
+                    </div>
+                </div>
+                    {/* <!-- Primary Navbar items --> */}
+                <div className="hidden md:flex items-center space-x-5">
+                    <a className="text-lg text-blue-600 transition duration-500 border-b-2 border-transparent hover:border-blue-600 hover:border-b-2" href="/">Home</a>
+
+                    <a className="text-lg text-blue-600 transition duration-500 border-b-2 border-transparent hover:border-blue-600 hover:border-b-2" href="/submission">Submit Story</a>
+
+                    <a className="text-lg text-blue-600 transition duration-500 border-b-2 border-transparent hover:border-blue-600 hover:border-b-2" href="/past-winners">Past Winners</a>
+
+                    <a className="text-lg text-blue-600 transition duration-500 border-b-2 border-transparent hover:border-blue-600 hover:border-b-2" href="/contact-us">Contact Us</a>
+
+                    <a className="text-lg text-blue-600 transition duration-500 border-b-2 border-transparent hover:border-blue-600 hover:border-b-2" href="/about-us">About Us</a>
+                    <div>
+                        <a href="https://www.facebook.com/The-Betty-Award-108114242034321" target="_blank" className="text-blue-600 hover:border-blue-600 text-xl mr-2"><i aria-hidden="true" className="fa fa-facebook-square"></i></a>
+                        <a href="https://www.instagram.com/p/CiAMCugOtry/?igshid=YmMyMTA2M2Y=" target="_blank" className="text-blue-600 hover:border-blue-600 text-lg"><i aria-hidden="true" className="fa fa-instagram"></i></a>
+
+                    </div>
+                </div>
+
+
+                {/* <!-- Mobile menu button --> */}
+                <div className="md:hidden flex items-center">
+                    <button className="outline-none mobile-menu-button">
+                        <svg className=" w-6 h-6 text-gray-500"
+                             x-show="!showMenu"
+                             fill="none"
+                             strokeLinecap="round"
+                             strokeLinejoin="round"
+                             strokeWidth="2"
+                             viewBox="0 0 24 24"
+                             stroke="currentColor">
+                            <path d="M4 6h16M4 12h16M4 18h16"></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+        {/* <!-- mobile menu --> */}
+        <div className="hidden mobile-menu">
+            <ul className="">
+                <li><a href="/" className="block text-sm px-2 py-4">Home</a></li>
+                <li><a href="/submission" className="block text-sm px-2 py-4 hover:bg-blue-600 transition duration-100">Submit a Story</a></li>
+                <li><a href="/past-winners" className="block text-sm px-2 py-4 hover:bg-blue-600 transition duration-100">Past Winners</a></li>
+                <li><a href="/contact-us" className="block text-sm px-2 py-4 hover:bg-blue-600 transition duration-100">Contact Us</a></li>
+                <li><a href="/about-us" className="block text-sm px-2 py-4 hover:bg-blue-600 transition duration-100">About Us</a></li>
+            </ul>
+        </div>
+        {/* <script>
+            const btn = document.querySelector("button.mobile-menu-button");
+            const menu = document.querySelector(".mobile-menu");
+
+            btn.addEventListener("click", () => {
+                menu.classList.toggle("hidden");
+            });
+        </script> */}
+    </nav>
+
+    <div className="flex-grow">
+      {children}
+    </div>
+
+    <footer className="w-full text-center bg-gray-200">
+        <div className="pt-4 text-sm">
+            © 2026 The Betty Award
+        </div>
+        <div className="pb-4">
+            <span className="text-sm">
+                Follow us on social media: 
+            </span>
+
+             <a href="https://www.facebook.com/The-Betty-Award-108114242034321" target="_blank" className="text-blue-600 hover:border-blue-600 text-xl mr-2"><i aria-hidden="true" className="fa fa-facebook-square"></i></a> 
+             <a href="https://www.instagram.com/p/CiAMCugOtry/?igshid=YmMyMTA2M2Y=" target="_blank" className="text-blue-600 hover:border-blue-600 text-lg"><i aria-hidden="true" className="fa fa-instagram"></i></a>
+        </div>
+    </footer>
+
+    {/* <script src="https://js.stripe.com/v3/"></script> */}
+    {/* <script src="~/lib/jquery/dist/jquery.min.js"></script> */}
+
+    {/* <script src="~/js/data.min.js" asp-append-version="true"></script> */}
+    {/* <script src="~/js/site220630.min.js" asp-append-version="true"></script> */}
+
+    {/* @await RenderSectionAsync("Scripts", required: false) */}
+  </body>
+     </html>
   );
 }
--- submission-site/app/page.tsx
+++ submission-site/app/page.tsx
@@ -42,7 +42,113 @@ export default function HomePage() {
     </div>
   </div>
 
+  
+  <div className="grid grid-cols-1 md:grid-cols-bg-pink-6002 gap-2 mx-auto max-w-7xl mt-2">
 
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 col-span-2 ">
+
+          <div className="">
+              <img 
+                   className="h-full"
+                   src="https://d30tirrb1lsi4u.cloudfront.net/public/site2.jpg"
+                   alt=""/>
+          </div>
+            <div className="p-8 bg-blue-600">
+                <h2 className="mb-1 text-xl font-bold text-white uppercase">The upcoming deadlines are as follows:</h2>
+                <p className="text-white text-sm mb-2">
+                    Summer 2026 Contest deadline is June 1, 2026. Winners will be announced on July 31, 2026.
+                </p>
+                <p className="text-white text-sm mb-2">
+                    Fall 2026 Contest deadline is October 3, 2026. Winners will be announced on November 1, 2026.
+                </p>
+                <p className="text-white text-sm mb-2">
+                    Winter 2027 Contest deadline is January 9, 2027. Winners will be announced on March 1, 2027.
+                </p>
+                <p className="text-white text-sm mb-2">
+                    Spring 2027 Contest deadline is April 3, 2027. Winners will be announced on May 29, 2027.
+                </p>
+            </div>
+
+
+          <div className="hidden md:block">
+              <img className="h-full" src="https://d30tirrb1lsi4u.cloudfront.net/public/site1.jpg" alt=""/>
+          </div>
+
+          <div className=" text-center bg-pink-600 py-14">
+              <h2 className="mb-3 text-3xl font-bold text-white uppercase">
+                  Prizes
+                  <i aria-hidden="true" className="fas fa-award"></i>
+              </h2>
+              <div className="mb-2 text-xl text-white uppercase">1st - $300 usd</div>
+              <div className="mb-2 text-xl text-white uppercase">2nd - $200 usd</div>
+              <div className="mb-2 text-xl text-white uppercase">3rd - $100 usd</div>
+              <div className="mb-2 text-white mx-2">All prizes are distributed electronically via Zelle, Venmo, or mailed to contestant upon request.</div>
+          </div>
+      </div>
+
+      <div className="flex flex-row col-span-2 bg-gray-200 justify-center py-5">
+          <div className="px-4 py-8 md:px-8">
+              <h1 className="mb-2 text-3xl font-extrabold uppercase text-pink-600">
+                  <i aria-hidden="true" className="fas fa-medal"></i>
+                  The Winter 2026 Winners Are
+                  <i aria-hidden="true" className="fas fa-medal"></i>
+              </h1>
+
+              <div className="py-3">
+                    <div className="font-bold">1st Place:  Finley Adamson</div>
+                          <div className="underline text-blue-601 hover:text-blue-400 story">
+                        <a href="https://betty-awards-uploads-prod.s3.us-east-2.amazonaws.com/The+Dive%2C+Winter+2025+FIRST+PLACE.pdf" target="_blank">The Dive</a>
+                          </div>
+                      </div>
+              <div className="py-3 ">
+                    <div className="font-bold">2nd Place: Charles Cheung</div>
+                          <div className="underline text-blue-601 hover:text-blue-400">
+                        <a href="https://betty-awards-uploads-prod.s3.us-east-2.amazonaws.com/Whiskers+on+the+Silk+Road.pdf" target="_blank">Whiskers on the Silk Road</a>
+                          </div>
+                      </div>
+
+              <div className="py-3">
+                    <div className="font-bold">3rd Place: Harley G Gallagher</div>
+                          <div className="underline text-blue-601 hover:text-blue-400">
+                        <a href="https://betty-awards-uploads-prod.s3.us-east-2.amazonaws.com/Daphne+(A+Greek+Mythology+Story)+WINTER+2025+THIRD+PLACE.pdf" target="_blank">Daphne (A Greek Mythology Story)</a>
+                          </div>
+                    </div>
+            
+          </div>
+      </div>
+
+
+      <div className="grid sm:grid-cols-1 md:grid-cols-3 col-span-2 gap-2 mb-2">
+
+          <div className="px-4 py-8 md:px-8 bg-gray-200">
+              <h2 className="mb-1 text-xl font-bold uppercase text-blue-600">How To Enter</h2>
+              <p className="mb-4">
+                  To enter, simply fill out our {" "}
+                  <a href="#" className="text-blue-600 underline hover:text-blue-400">
+                      online
+                      form.
+                  </a>
+              </p>
+          </div>
+
+          <div className="px-4 py-8 md:px-8 bg-gray-200">
+              <h2 className="mb-1 text-xl font-bold uppercase text-blue-600">Reading Fee</h2>
+              <p>
+                  There is a $20 US reading fee per story. <br />
+                  We accept all major credit and debit cards. Please reach out with any questions on our <a href="/contact-us">contact page.</a>
+              </p>
+          </div>
+
+          <div className="px-4 py-8 md:px-8 bg-gray-200">
+              <h2 className="mb-1 text-xl font-bold uppercase text-blue-600">Publication</h2>
+              <p>
+                  The Winter 2026 winners will be announced on our website and social media accounts on March 1, 2026. 
+              </p>
+          </div>
+      </div>
+
+      </div>
     </main>
+
   );
 }
