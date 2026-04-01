0b1b1930d5b26e54e6ec6da2f8d6d776f98830a3
Ricco Amezcua <ricco.amezcua@gmail.com>
2026-04-01 11:38:01 -0500

    Add main page header

--- submission-site/app/page.tsx
+++ submission-site/app/page.tsx
@@ -1,7 +1,9 @@
 export default function HomePage() {
   return (
     <main>
-      <div>Submission site</div>
+      <p className="w-full py-6 px-4 med:text-lg italic text-center bg-gray-100">
+          Recognizing and Celebrating Young Authors Since 2006!
+      </p>
     </main>
   );
 }
